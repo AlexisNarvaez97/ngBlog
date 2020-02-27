2113c1df9f55feb26051af1566599e52d80e9db0
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-02-26 22:21:25 -0600

    Agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PostComponent } from './components/posts/post/post.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./components/pages/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "post/:id",
+    component: PostComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewPostComponent } from './components/posts/new-post/new-post.component';
+import { NewPostModule } from './components/posts/new-post/new-post.module';
+import { PostComponent } from './components/posts/post/post.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NewPostModule
   ],
   providers: [],
   bootstrap: [AppComponent]
